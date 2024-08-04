2c61eb5c6185a1f1b777c1b46dc8e7ae2bdbf944
Softlaptop <Softlaptop@DESKTOP-3CE5M2E>
2024-08-04 05:12:53 +0300

    assignment

---
+++ Assignment/BubbleSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BubbleSortedArray
+    {
+        public static void BubbleSortv<T> (T[]array) where T : IComparable<T> 
+        {
+            if (array != null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    bool flag = false;
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].CompareTo(array[j+1]) == 1)
+                        {
+                            T temp = array[j];
+                            array[j] = array[j+1];
+                            array[j+1] = temp;
+                            flag = true;
+                        }
+                    }
+                    if (!flag)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assignment/Range.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Range <T> where T : IComparable<T>
+    {
+
+        public T minimum { get; set; }
+        public T maximum { get; set; }
+        public Range(T minimum, T maximum)
+        {
+            this.minimum = minimum;
+            this.maximum = maximum;
+        }
+
+        public bool IsInRange(T value )
+        {
+            if (value.CompareTo(minimum) >= 0 && value.CompareTo(maximum) <= 0)
+                return true;
+            return false;
+        }
+
+        public T Length()
+        {
+            dynamic mn = minimum;
+            dynamic mx = maximum;
+            return mx - mn;
+        }
+    }
+}
